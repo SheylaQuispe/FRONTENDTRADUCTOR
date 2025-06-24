9fa4b178110bbfd350bc98bdbcaa5295b51d4011
SheylaQuispe <u201620209@upc.edu.pe>
2025-06-24 15:42:23 -0500

    ACTUALIZACION

--- src/app/components/pago/listarpago/listarpago.component.ts
+++ src/app/components/pago/listarpago/listarpago.component.ts
@@ -18,7 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
 export class ListarpagoComponent implements OnInit{
   dataSource: MatTableDataSource<Pago> = new MatTableDataSource();
 
-  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5','c6','c7'];
+  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5','c6','c7','c8'];
 
   constructor(private pS: PagoService){}
   ngOnInit(): void {
--- src/app/models/pago.ts
+++ src/app/models/pago.ts
@@ -1,7 +1,11 @@
+import { Suscripcion } from "./suscripcion"
+
+
 export class Pago{
     idPago:number=0
     monto:number=0
     metodo:String=""
     fechaPago:Date= new Date()
     estado:boolean=false
+    idSuscripcion: Suscripcion = new Suscripcion()
 }
